35c17b8d3cc35b89a64ecd1ff65989f72b3e0999
ITurres <emanueliturres.co@gmail.com>
2024-01-16 17:05:54 -0300

    Feat: Add react-route to '/greetings/random_greeting' at 'App.js'

--- app/javascript/App.js
+++ app/javascript/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Greeting from './components/Greeting';
 
 function App() {
   return (
     <>
-      <Greeting />
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/greetings/random_greeting">Random Greeting</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Route path="/greetings/random_greeting" component={Greeting} />
+        </div>
+      </Router>
     </>
   );
 }
