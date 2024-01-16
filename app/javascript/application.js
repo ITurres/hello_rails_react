25d538e12c154e124dbed0b1c83fb28be931fa45
ITurres <emanueliturres.co@gmail.com>
2024-01-16 14:45:43 -0300

    Chore: update to react 'createRoot'

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { BrowserRouter } from 'react-router-dom';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { Provider } from 'react-redux';
+
 import App from './App';
 import store from './redux/configureStore';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
 
 root.render(
   <Provider store={store}>
